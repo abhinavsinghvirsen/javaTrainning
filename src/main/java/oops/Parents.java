515e299b119fdc3644cd9eea45852f86d747e70f
abhinav-fairsettle <abhinavxeon@gmail.com>
2023-06-14 23:57:11 +0530

    class number 1 commit

---
+++ src/main/java/oops/Parents.java
@@ -0,0 +1,31 @@
+package oops;
+
+public class Parents {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+}
+
+
+class FlyingMachine{
+
+
+
+
+  int a ;
+  static int b;//
+
+   String name;
+
+   static String fathername;
+
+
+
+
+}
+
---
+++ src/main/java/oops/Student.java
@@ -0,0 +1,37 @@
+package oops;
+
+import java.util.Date;
+
+public class Student {
+
+     String name;
+     int roll;
+
+     public void reading(){
+
+
+     }
+
+     public void eating(){
+
+
+     }
+
+
+
+
+
+    public static void main(String[] args) {
+
+    Student abhinav = new Student();
+
+    abhinav.reading();
+    abhinav.eating();
+        int roll = abhinav.roll;
+       // abhinav.name
+
+
+    }
+
+
+}
