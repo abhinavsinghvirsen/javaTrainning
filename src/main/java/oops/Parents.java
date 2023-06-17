391f41fad3f95a13eefd56cadc3499f86cf68d99
abhinav-fairsettle <abhinavxeon@gmail.com>
2023-06-17 10:27:11 +0530

    first commit from aprita and priyo
    appriciated

--- src/main/java/oops/Parents.java
+++ src/main/java/oops/Parents.java
@@ -5,6 +5,7 @@ public class Parents {
     public static void main(String[] args) {
 
  System.out.println("my first change") ;
+        System.out.println("Good to know | you have learn to commit");
 
 
     }
