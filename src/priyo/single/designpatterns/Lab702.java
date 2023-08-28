266e3ee23ca77dfd2e7632022ef1665bc94a3bcd
arpitalenka <105102000+arpitalenka@users.noreply.github.com>
2023-08-28 18:33:22 +0530

    commit

---
+++ src/priyo/single/designpatterns/Lab702.java
@@ -0,0 +1,28 @@
+package single.designpatterns;
+
+public class Lab702 {
+
+    public static void main(String[] args) {
+
+        Hello h = Hello.getHello() ;
+
+        System.out.println(h) ;
+
+        System.out.println(Hello.getHello()) ;
+
+        System.out.println(Hello.getHello()) ;
+    }
+}
+
+
+class Hello {
+
+    private static Hello INS = new Hello() ;
+
+    private Hello() { }
+
+    public static Hello getHello() {
+
+        return  INS ;
+    }
+}
