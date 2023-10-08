401eecd86c2a825d215218e58f4bca38c4fa113c
abhinav-fairsettle <abhinavxeon@gmail.com>
2023-10-08 22:11:42 +0530

    join

--- src/abhinav/threads/MAinTest.java
+++ src/abhinav/threads/MAinTest.java
@@ -2,23 +2,57 @@ package abhinav.threads;
 
 public class MAinTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("hi "+Thread.currentThread().getName());
-        Th obj= new Th();
-        obj.setName("thread007");
-        obj.start();
+        JH obj1= new JH();
+        obj1.setName("A ");
+
+        MH obj2= new MH();
+        obj2.setName("B ");
+
+        KA obj3= new KA();
+        obj3.setName("C ");
+
+        obj1.start();
+        obj1.join();
+
+
+        obj2.start();
+        obj2.join();
+        obj3.start();
 
 
         System.out.println("bye "+Thread.currentThread().getName());
     }
 }
 
-class Th extends Thread{
+class JH extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + i);
+        }
+    }
+}
+class MH extends Thread{
 
     @Override
     public void run() {
 
-        System.out.println(Thread.currentThread().getName()+(5/0));
+        for (int i=0; i<10;i++){
+            System.out.println(Thread.currentThread().getName()+i);
+        }
     }
 }
 
+class KA extends Thread{
+
+    @Override
+    public void run() {
+
+        for (int i=0; i<10;i++){
+            System.out.println(Thread.currentThread().getName()+i);
+        }
+    }
+}
